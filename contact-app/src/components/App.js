90c24cdc92bd5de6578f96ea66cd083c42e90060
Sadiq <sadiq78692431@gmail.com>
2023-04-25 21:00:23 +0530

    Printed the Header

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -1,9 +1,17 @@
+import React from "react";
 import './App.css';
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList"
 
 function App() {
     return (
     
-      <div>Hello world!</div>
+      <div>
+        <Header></Header>
+        {/* <AddContact></AddContact>
+        <ContactList></ContactList> */}
+      </div>
     
   );
 }
